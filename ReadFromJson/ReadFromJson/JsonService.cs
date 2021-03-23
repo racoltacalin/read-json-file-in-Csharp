d5584c5185b87aeeef2ed29ef55c7e84ad3dceee
Clain <Calin.Racolta@student.utcluj.ro>
2021-03-23 09:24:16 +0200

    add the project

---
+++ ReadFromJson/ReadFromJson/JsonService.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace ReadFromJson
+{
+    public class JsonService
+    {
+        public static void ReadJsonFile(string jsonFileIn)
+        {
+            dynamic jsonFile = JsonConvert.DeserializeObject(File.ReadAllText(jsonFileIn));
+
+            foreach (var item in jsonFile)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ReadFromJson/ReadFromJson/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace ReadFromJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pathToJson = Path.Combine("myFile.json");
+            JsonService.ReadJsonFile(pathToJson);
+        }
+    }
+}
